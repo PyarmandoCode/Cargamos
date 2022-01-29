fe4a31712cd61d619ea14b98546842ad018530da
armando.eu.ruiz@gmail.com <armando.eu.ruiz@gmail.com>
2022-01-29 12:32:04 -0500

    basededatos

---
+++ sesion4/base_de_datos.py
@@ -0,0 +1,8 @@
+empleados=[
+   {'codigo':'2334455','apellido':'Ruiz','nombre':'Armando','sueldo':7000,'area':'sistemas'},
+   {'codigo':'2334456','apellido':'Gomez','nombre':'Pedro','sueldo':8800,'area':'sistemas'},
+   {'codigo':'2334457','apellido':'Cespedes','nombre':'Javier','sueldo':9000,'area':'contabilidad'},
+   {'codigo':'2334458','apellido':'Linares','nombre':'Oscar','sueldo':12000,'area':'contabilidad'},
+   {'codigo':'2334459','apellido':'Palomino','nombre':'Maria','sueldo':170000,'area':'rrhh'},
+   {'codigo':'2334460','apellido':'Prado','nombre':'Carlos','sueldo':270000,'area':'estadistica'}
+]
