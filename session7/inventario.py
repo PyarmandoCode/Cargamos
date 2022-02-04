5cf654ee4f73aab7c12f3ad50e3f35c50f7db770
armando.eu.ruiz@gmail.com <armando.eu.ruiz@gmail.com>
2022-02-04 17:16:24 -0500

    Ejercicio POO

---
+++ session7/inventario.py
@@ -0,0 +1,7 @@
+# todo crear un programa utilizando Poo que me permita Listar
+# todo los productos que estan suspendidos
+# todo crear los atributos de acuerdo a los campos del excel
+# todo hacer un metodo que nos permita cargar la informacion 
+# todo del csv
+# todo crear otro metodo que nos liste los productos suspendidos
+# todo POO
