3f7480d330135171a22f186cad62af22f151060e
armando.eu.ruiz@gmail.com <armando.eu.ruiz@gmail.com>
2022-02-04 18:03:36 -0500

    Trabajo

--- session7/inventario.py
+++ session7/inventario.py
@@ -5,3 +5,32 @@
 # todo del csv
 # todo crear otro metodo que nos liste los productos suspendidos
 # todo POO
+
+import csv
+class Productos:
+    def __init__(self,IdProducto="",NombreProducto="",Proveedor="",Categoría="",CantidadPorUnidad="",PrecioUnidad="",UnidadesEnExistencia="",UnidadesEnPedido="",NivelNuevoPedido="",Suspendido=""):
+        self.IdProducto=IdProducto,
+        self.NombreProducto=NombreProducto,
+        self.Proveedor=Proveedor,
+        self.Categoría=Categoría,
+        self.CantidadPorUnidad=CantidadPorUnidad,
+        self.PrecioUnidad=PrecioUnidad,
+        self.UnidadesEnExistencia=UnidadesEnExistencia,
+        self.UnidadesEnPedido=UnidadesEnPedido,
+        self.NivelNuevoPedido=NivelNuevoPedido,
+        self.Suspendido=Suspendido
+        self.productos=[]
+        
+    def cargar_productos(self):
+        with open('session7/productos.csv','r') as data_cargamos:
+            csv_leer=csv.reader(data_cargamos)
+            self.productos=list(csv_leer)
+            suspendidos=[]
+            for self.IdProducto,self.NombreProducto,self.Proveedor,self.Categoría,self.CantidadPorUnidad,self.PrecioUnidad,self.UnidadesEnExistencia,self.UnidadesEnPedido,self.NivelNuevoPedido,self.Suspendido in self.productos:
+                if self.Suspendido=='VERDADERO':
+                    suspendidos.append((self.NombreProducto,self.Suspendido))
+            return suspendidos    
+        
+            
+data=Productos()
+print(data.cargar_productos() ) 
