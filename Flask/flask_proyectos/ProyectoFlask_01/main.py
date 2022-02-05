bf553286763059476a4439a393a9f336d982172a
armando.eu.ruiz@gmail.com <armando.eu.ruiz@gmail.com>
2022-02-05 11:59:42 -0500

    Imagenes en Flask

--- Flask/flask_proyectos/ProyectoFlask_01/main.py
+++ Flask/flask_proyectos/ProyectoFlask_01/main.py
@@ -5,11 +5,11 @@ from flask import Flask,render_template
 app=Flask(__name__)
 
 data_cargamos=[
-    {"nombre":"Alicia","apellido":"Guerrero"},
-    {"nombre":"Carlos","apellido":"Aguilar"},
-    {"nombre":"Ingrid","apellido":"Guerrero"},
-    {"nombre":"Javier","apellido":"Catalan"},
-    {"nombre":"Lhordes","apellido":"Catln"},
+    {"nombre":"Alicia","apellido":"Guerrero" ,"picture":"https://www.baarty.com/articulos/wp-content/uploads/2013/04/motivando-empleado-bares.jpg"},
+    {"nombre":"Carlos","apellido":"Aguilar","picture":"https://www.bbva.com/wp-content/uploads/2013/03/carlos-torres-vila-ceo-bbva-1024x885.jpg"},
+    {"nombre":"Ingrid","apellido":"Guerrero","picture":"https://www.clasibo.com/aviso_images/190626_041735.jpg"},
+    {"nombre":"Javier","apellido":"Catalan","picture":"https://factorialhr.es/wp-content/uploads/2019/12/20130507/empleados-felices-portada.jpg"},
+    {"nombre":"Lhordes","apellido":"Catln","picture":"https://static9.depositphotos.com/1594308/1163/i/950/depositphotos_11632583-stock-photo-successful-accountant.jpg"},
 ]
 
 #todo creando mi primera ruta
