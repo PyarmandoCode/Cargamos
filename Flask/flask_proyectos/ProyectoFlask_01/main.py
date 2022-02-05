b8cf27a5298bf63035546602cdc163c76bf5257d
armando.eu.ruiz@gmail.com <armando.eu.ruiz@gmail.com>
2022-02-05 11:34:36 -0500

    Jinja

--- Flask/flask_proyectos/ProyectoFlask_01/main.py
+++ Flask/flask_proyectos/ProyectoFlask_01/main.py
@@ -4,13 +4,30 @@ from flask import Flask,render_template
 #todo creando el proyecto
 app=Flask(__name__)
 
+data_cargamos=[
+    {"nombre":"Alicia","apellido":"Guerrero"},
+    {"nombre":"Carlos","apellido":"Aguilar"},
+    {"nombre":"Ingrid","apellido":"Guerrero"},
+    {"nombre":"Javier","apellido":"Catalan"},
+    {"nombre":"Lhordes","apellido":"Catln"},
+]
+
 #todo creando mi primera ruta
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/cargamos')
+def cargamos():
+    return render_template("cargamos.html",alumnos=data_cargamos)
+
+@app.route('/acercade')
+def acercade():
+    return render_template("acercade.html")
+
     
 #todo ejecutando el servidor        
-app.run(port="8080",debug=True)    
+app.run(debug=True)    
 
 
 
