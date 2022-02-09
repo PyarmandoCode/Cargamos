46767f93bb51515a21e431047b288438cd260054
armando.eu.ruiz@gmail.com <armando.eu.ruiz@gmail.com>
2022-02-09 16:02:14 -0500

    sqlalchemy

--- Proyecto_Monolitico/app.py
+++ Proyecto_Monolitico/app.py
@@ -1,6 +1,12 @@
 from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy
+
 
 app=Flask(__name__)
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:cargamos2022@localhost/bdcargamos'
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
