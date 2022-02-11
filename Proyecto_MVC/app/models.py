fc279cdd298dde8b1331740beaa27eaad91b7ff1
armando.eu.ruiz@gmail.com <armando.eu.ruiz@gmail.com>
2022-02-11 15:53:24 -0500

    crud

--- Proyecto_MVC/app/models.py
+++ Proyecto_MVC/app/models.py
@@ -1,7 +1,7 @@
 from app import db
 
 class Grupos(db.Model):
-    codgrupo=db.Column(db.String(3),primary_key=True)
+    codgrupo=db.Column(db.Integer,primary_key=True,autoincrement=True)
     nombre_grupo=db.Column(db.String(50))
     estado=db.Column(db.Integer)
     
--- Proyecto_MVC/app/routes.py
+++ Proyecto_MVC/app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request,redirect
 #todo importando la aplicacion y la conexion a la base de datos
 from app import app,db
 #todo importando el modelo = tabla
@@ -11,6 +11,30 @@ def index():
     gps= Grupos.query.all()
     template_name="index.html"
     return render_template(template_name,grupos=gps)
+
+
+@app.route('/agregar' ,methods=['POST'])
+def agregar():
+    if request.method == "POST":
+        #todo recogiendo los varlores del formulario
+        form=request.form
+        nombre_grupo=form.get("grupo")
+        estado=form.get("estado")
+        if not nombre_grupo or estado:
+            #todo se agregan los datos del formulario a la clase
+            grupos=Grupos(nombre_grupo=nombre_grupo,estado=estado)
+            #todo se agrega a la tabla de mysql
+            db.session.add(grupos)
+            #todo commit insertar los valores a la tabla
+            db.session.commit()
+            #todo recarga la pagina con los nuevos valores =F5
+            return redirect('/')
+        return "termino la grabacion"
+            
+            
+        
+        
+        
 
 
     
